852b090652f209160967dd96e919dc329527a082
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2024-07-18 11:51:03 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 19.89%] - LeetPush

--- 74-Search-a-2D-Matrix.java
+++ 74-Search-a-2D-Matrix.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i = 0; i<matrix.length; i++){
             for(int ele: matrix[i]){
                 if(ele == target){
-                    return true;
+                    return true; // true
                 }else if(ele> target){
-                    break;
+                    break; 
                 }
             }
         }
